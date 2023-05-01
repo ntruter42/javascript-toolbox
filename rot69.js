d7f2e2b913a2c08b6d135c56b4d6128034a586ef
Nicholas Truter <ntruter42@gmail.com>
2023-05-01 23:49:40 +0200

    Update rot69.js - fixed minor command-line pramater (argv) bug, added negative shift functionality

--- rot69.js
+++ rot69.js
@@ -9,20 +9,22 @@ function rot69(str, shift) {
 		const char = str[i];
 		const charIndex = alphabet.indexOf(char.toLowerCase());
 		const numIndex = numbers.indexOf(char.toLowerCase());
-		
+
 		// indexOf() returns -1 if char is not found in string
 		if (charIndex !== -1) {
 			// Shift the letter by the specified number of places
-			const shiftedIndex = (charIndex + shift) % 26;
+			let shiftedIndex = (charIndex + shift) % 26;
+			if (shiftedIndex < 0)
+				shiftedIndex += 26;
 			const shiftedChar = alphabet[shiftedIndex];
-
 			// Preserve the case of the original character
 			result += char === char.toUpperCase() ? shiftedChar.toUpperCase() : shiftedChar;
 		} else if (numIndex !== -1) {
 			// Shift the number by the specified number of places
-			const shiftedIndex = (numIndex + shift) % 10;
+			let shiftedIndex = (numIndex + shift) % 10;
+			if (shiftedIndex < 0)
+				shiftedIndex += 10;
 			const shiftedNum = numbers[shiftedIndex];
-
 			result += shiftedNum;
 		} else {
 			// Character is not a letter or number, so just append it to the result
@@ -33,13 +35,16 @@ function rot69(str, shift) {
 	return result;
 }
 
-if (process.argv.length < 5) {
+if (process.argv.length < 4) {
 	console.error('Usage: node rot69.js <filename> <shift>');
+	for (let arg in process.argv) {
+		console.log(arg + ' - ' + process.argv[arg]);
+	}
 	process.exit(1);
 }
 
 const filename = process.argv[2];
-const shift = parseInt(process.argv[4]);
+const shift = parseInt(process.argv[3]);
 fs.readFile(filename, 'utf8', (err, data) => {
 	if (err) {
 		console.error(`Error reading file: ${err}`);

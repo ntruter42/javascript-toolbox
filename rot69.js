918a2400a900a4924622c408cd0622ebaf81d2f3
Nicholas Truter <ntruter42@gmail.com>
2023-05-02 00:03:23 +0200

    Update rot69.js - added special characters functionality

--- rot69.js
+++ rot69.js
@@ -3,12 +3,14 @@ const fs = require('fs');
 function rot69(str, shift) {
 	const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 	const numbers = '0123456789';
+	const special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~\\\"";
 	let result = '';
 
 	for (let i = 0; i < str.length; i++) {
 		const char = str[i];
 		const charIndex = alphabet.indexOf(char.toLowerCase());
-		const numIndex = numbers.indexOf(char.toLowerCase());
+		const numIndex = numbers.indexOf(char);
+		const specialIndex = special.indexOf(char);
 
 		// indexOf() returns -1 if char is not found in string
 		if (charIndex !== -1) {
@@ -19,14 +21,27 @@ function rot69(str, shift) {
 			const shiftedChar = alphabet[shiftedIndex];
 			// Preserve the case of the original character
 			result += char === char.toUpperCase() ? shiftedChar.toUpperCase() : shiftedChar;
-		} else if (numIndex !== -1) {
+		}
+
+		else if (numIndex !== -1) {
 			// Shift the number by the specified number of places
 			let shiftedIndex = (numIndex + shift) % 10;
 			if (shiftedIndex < 0)
 				shiftedIndex += 10;
 			const shiftedNum = numbers[shiftedIndex];
 			result += shiftedNum;
-		} else {
+		}
+
+		else if (specialIndex !== -1) {
+			// Shift the special symbol by the specified number of places
+			let shiftedIndex = (specialIndex + shift) % 32;
+			if (shiftedIndex < 0)
+				shiftedIndex += 32;
+			const shiftedSpecial = special[shiftedIndex];
+			result += shiftedSpecial;
+		}
+
+		else {
 			// Character is not a letter or number, so just append it to the result
 			result += char;
 		}
